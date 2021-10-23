a17f4ef986adabc21cac3b1880c614d91c27e00d
acea68 <alancea01@gmail.com>
2021-10-22 23:40:50 -0500

    Added approach #2 to reversestring function.

--- reversestring/new_index.js
+++ reversestring/new_index.js
@@ -1,14 +1,44 @@
-function reverse(str) {
-    var split_str = str.split('')
-    // console.log(split_str)
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'elppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// Method #1: create function "reverse", with an input of a string. 
+
+// function reverse(str) {
+//     var split_str = str.split('') // Then create a variable. Apply method 
+//     // "split," to split up string into individual elements, creating a list. 
+   
+//     // console.log(split_str)
+//     // prints initial string that has been split
     
-    let reversed_list = split_str.reverse()
-    // console.log(reversed_list)
+//     let reversed_list = split_str.reverse() // create new variable2, apply method "reverse" to it. 
+//     // Output is same length list, with elements in reverse order. 
+    
+//     // console.log(reversed_list)
 
-    var joined_str = reversed_list.join('')
-    // console.log(joined_str)
+//     var joined_str = reversed_list.join('')
+//     // console.log(joined_str)
 
-    return joined_str
+//     return joined_str
+// }
+
+// module.exports = reverse;
+
+// var input = "hello"
+// console.log(input)
+// var ans = reverse(input)
+// console.log(ans)
+
+// ----------------------
+
+// Method #2:
+
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
 }
 
 module.exports = reverse;
