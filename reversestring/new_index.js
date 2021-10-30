b4fea60e942e5c47b5b2a9ab403426c41ccbfcb2
acea68 <alancea01@gmail.com>
2021-10-29 22:22:43 -0500

    added chunk exercise

--- reversestring/new_index.js
+++ reversestring/new_index.js
@@ -6,30 +6,48 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// ---------------------------------
+
 // Method #1: create function "reverse", with an input of a string. 
 
 // function reverse(str) {
-//     var split_str = str.split('') // Then create a variable. Apply method 
+//   let split_str = str.split('')
+//   let rev_list = split_str.reverse()
+//   let join_str_rev = rev_list.join('')
+
+//   --OR-- return str.split('').reverse().join('');
+
+//   return join_str_rev
+
+// }
+// let str = 'apple'
+// console.log(str)
+// let str_rev = reverse(str)
+// console.log(str_rev)
+
+// function reverse(str) {
+//   console.log("Input into the function: ",str)
+//   var split_str = str.split('') // create a variable. Apply method 
 //     // "split," to split up string into individual elements, creating a list. 
    
-//     // console.log(split_str)
-//     // prints initial string that has been split
+//     console.log("input string, now split into list: ",split_str)  // prints initial string that has been split
     
 //     let reversed_list = split_str.reverse() // create new variable2, apply method "reverse" to it. 
 //     // Output is same length list, with elements in reverse order. 
     
-//     // console.log(reversed_list)
+//     console.log("list is now reversed: ",reversed_list)
 
-//     var joined_str = reversed_list.join('')
-//     // console.log(joined_str)
+//     var joined_str = reversed_list.join('') // take reversed list, then using "join" method, 
+//     // it into a string again
+//     console.log("list is regrouped into a string: ",joined_str)
 
 //     return joined_str
 // }
 
 // var input = "hello"
-// console.log(input)
+// // console.log(input)
 // var ans = reverse(input)
-// console.log(ans)
+// // console.log(ans)
 
 // ----------------------
 
@@ -43,20 +61,22 @@
 // var ans = reverse(input)
 // console.log(ans)
 
+// -----------------------
+
 // Method #3:
 
-function reverse(str) {
-  let reversed = '';
+// function reverse(str) {
+//   let reversed = '';
 
-  for (let character of str) {
-    reversed = character + reversed;
-    // console.log(reversed)
-  }
+//   for (let character of str) {
+//     reversed = character + reversed;
+//     // console.log(reversed)
+//   }
 
-  return reversed;
-}
+//   return reversed;
+// }
 
-let new_var = "hello"
-console.log(reverse(new_var))
+// let new_var = "hello"
+// console.log(reverse(new_var))
 
-module.exports = reverse;
+// module.exports = reverse;
