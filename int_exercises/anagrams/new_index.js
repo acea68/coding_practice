52f75a0c6738de25f936886edf996a505d38acd5
acea68 <alancea01@gmail.com>
2021-11-01 21:43:32 -0500

    restructured folder. added method #2 for anagrams

--- int_exercises/anagrams/new_index.js
+++ int_exercises/anagrams/new_index.js
@@ -8,34 +8,63 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-  console.log('isequal: ',cleanString(stringA) === cleanString(stringB))
-  return cleanString(stringA) === cleanString(stringB);
-}
+// -------------------------------
 
-function cleanString(str) {
-  let s1 = str.replace(/[^\w]/g, '')
-  console.log("reduce to only strings: ", s1)
-  let s2 = s1.toLowerCase()
-  console.log("lower case: ", s2)
-  let s3 = s2.split('')
-  console.log("split: ", s3)
-  let s4 = s3.sort()
-  console.log("sort: ", s4)
-  let s5 = s4.join('');
-  console.log("join: ", s5)
-}
-console.log(anagrams('RAIL! SAFETY!', 'fairy tales'))
-module.exports = anagrams;
+// Method #1
 
 // function anagrams(stringA, stringB) {
+//   console.log('isequal: ',cleanString(stringA) === cleanString(stringB))
 //   return cleanString(stringA) === cleanString(stringB);
 // }
 
 // function cleanString(str) {
-//   return str
-//     .replace(/[^\w]/g, '')
-//     .toLowerCase()
-//     .split('')
-//     .sort()
-//     .join('');
+//   let s1 = str.replace(/[^\w]/g, '')
+//   console.log("reduce to only strings: ", s1)
+//   let s2 = s1.toLowerCase()
+//   console.log("lower case: ", s2)
+//   let s3 = s2.split('')
+//   console.log("split: ", s3)
+//   let s4 = s3.sort()
+//   console.log("sort: ", s4)
+//   let s5 = s4.join('');
+//   console.log("join: ", s5)
+// }
+
+// -------------------------------
+
+// Method #2
+
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+    console.log("aCharMap: ", aCharMap)
+    console.log("bCharMap: ", bCharMap)
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    console.log("char: ", char)
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+    console.log("charMap: ", charMap)
+  }
+
+  return charMap;
+}
+
+console.log(anagrams('Hello!1', '2holahi'))
+
+module.exports = anagrams;
