99ebb8374263bc3694044471204ac8e20bfb31a8
acea68 <alancea01@gmail.com>
2021-11-05 20:26:00 -0500

    added new approach to capitalize exercise.

--- int_exercises/capitalize/index.js
+++ int_exercises/capitalize/index.js
@@ -19,18 +19,39 @@
  * join array
   */
 
+// ---------------------------
+// Method #1
 
-function capitalize(str) { 
-  let storedArray = [];
+// function capitalize(str) { 
+//   let storedArray = [];
 
-  for (let word of str.split(' ')) {
-    storedArray.push(word[0].toUpperCase() + word.slice(1))
-  }  
-return storedArray.join(' ')
+//   for (let word of str.split(' ')) {
+//     storedArray.push(word[0].toUpperCase() + word.slice(1))
+//   }  
+// return storedArray.join(' ')
+// }
+
+// ---------------------------
+// Method #2
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    console.log('i: ', i)
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+      console.log('result: ', result)
+    } else {
+      result += str[i];
+      console.log('result: ', result)
+    }
+  }
+
+  return result;
 }
-
-capitalize('a short sentence') //--> 'A Short Sentence'
-capitalize('a lazy fox') //--> 'A Lazy Fox'
-capitalize('look, it is working!') //--> 'Look, It Is Working!'
+// console.log(capitalize('a short sentence')) //--> 'A Short Sentence'
+// console.log(capitalize('a lazy fox')) //--> 'A Lazy Fox'
+console.log(capitalize('look, it is working!')) //--> 'Look, It Is Working!'
 
 module.exports = capitalize;
