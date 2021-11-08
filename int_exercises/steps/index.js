2a1eb5341f16e223bffb39f0537899e16acb0a5f
acea68 <alancea01@gmail.com>
2021-11-07 19:13:11 -0600

    added approach #2 to steps

--- int_exercises/steps/index.js
+++ int_exercises/steps/index.js
@@ -17,31 +17,58 @@
 //       '### '
 //       '####'
 
-let numSteps = 3
-console.log("steps:")
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    // console.log('row: ', row)
-    let stair = '';
-    // console.log('1st stair:', stair)
+// ----------------------------------------
+// Method #1
 
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        // console.log('column: ', column)
-        stair += '#';
-        // console.log('2nd stair:', stair)
-
-      } else {
-        stair += ' ';
-        // console.log('3rd stair:', stair)
-      }
-    }
-
-    console.log(stair);
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
   }
-}
-steps(numSteps)
 
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';   
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
+}
+
+steps(10)
+
+// // ----------------------------------------
+// // Method #2
+
+
+// let numSteps = 3
+// console.log("steps:")
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     // console.log('row: ', row)
+//     let stair = '';
+//     // console.log('1st stair:', stair)
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         // console.log('column: ', column)
+//         stair += '#';
+//         // console.log('2nd stair:', stair)
+
+//       } else {
+//         stair += ' ';
+//         // console.log('3rd stair:', stair)
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
+// steps(numSteps)
 
 module.exports = steps;
