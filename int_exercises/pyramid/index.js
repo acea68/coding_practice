d3821addeb9b367d35edd2d25a7c5a776c8a7441
acea68 <alancea01@gmail.com>
2021-11-09 22:26:23 -0600

    added new approach to pyramid

--- int_exercises/pyramid/index.js
+++ int_exercises/pyramid/index.js
@@ -14,30 +14,55 @@
 //       ' ### '
 //       '#####'
 
+// -----------------------------------
+// Method #2
 
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2);
-
-  for (let row = 0; row < n; row++) {
-    let level = '';
-    console.log('row:', row)
-    console.log('level:', level)
-
-    for (let column = 0; column < 2 * n - 1; column++) {
-      console.log('column:', column)
-      if (midpoint - row <= column && midpoint + row >= column) {
-        level += '#';
-        console.log('nested level:', level)
-      } else {
-        level += ' ';
-        console.log('nested level:', level)
-
-      }
-    }
-
-    console.log(level);
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
   }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
 }
 
-pyramid(4)
+pyramid(5)
 module.exports = pyramid;
+
+// -----------------------------------
+// Method #1
+
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//     console.log('row:', row)
+//     console.log('level:', level)
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       console.log('column:', column)
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//         console.log('nested level:', level)
+//       } else {
+//         level += ' ';
+//         console.log('nested level:', level)
+
+//       }
+//     }
+
+//     console.log(level);
+//   }
+// }
