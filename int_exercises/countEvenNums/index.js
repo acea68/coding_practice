b8da6f2839399716f27caa456aa5fb17928b6331
acea68 <alancea01@gmail.com>
2021-11-27 13:18:28 -0600

    added new exercise createArray to countEvenNums

--- int_exercises/countEvenNums/index.js
+++ int_exercises/countEvenNums/index.js
@@ -9,5 +9,35 @@ let countEvenNums = function (array) {
     }
     return evenNumCount
 }
+// let inputArray = [3,2,444,50,-2,333,4,-50]
 
-console.log(countEvenNums([3,2,4,5,2,3,4,-50]))
+// console.log(countEvenNums(inputArray))
+
+/* Exercise 3.2
+Write a JavaScript program to find the number of even values up to a given number. 
+*/
+
+let createArray = function(numLim) {
+    let newArray = []
+    for (let i = 1; i <= numLim; i++) { // if you want to include '0' as part of the count, set i=0
+        newArray.push(i)
+    }
+    return newArray
+}
+console.log(countEvenNums(createArray(10)))
+
+
+// let evenNumLimit = function (array, limit) {
+//     let evenNumCount = 0 
+//     for (let num of array) {
+//         num % 2 === 0 && num <= limit ? evenNumCount++ : evenNumCount 
+//     }
+//     return evenNumCount
+// }
+// let inputArray = [3,2,444,50,-2,333,4,-50]
+// console.log(evenNumLimit(inputArray, 100)) 
+
+// let countLimit = function (arr, limit) {
+//     return countEvenNums(arr) <= limit ? countEvenNums(arr) : limit
+// }
+// console.log(countLimit(inputArray, 4))
