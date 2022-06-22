817ea52e116c4cfce0c5a4417025e64ce780ad61
acea68 <alancea01@gmail.com>
2022-06-22 08:39:20 -0500

    updates app

--- terminalAppTodo/todo.js
+++ terminalAppTodo/todo.js
@@ -1,19 +1,24 @@
 let input = prompt('What would you like to do?').toLowerCase();
 let todos = ['study coding problems', 'practice coding apps'];
-while( input !== 'quit' && input !== 'q') {
+while (input !== 'quit' && input !== 'q') {
   if (input === 'list') {
     console.log('********************');
     for (let i = 0; i < todos.length; i++) {
       console.log(`${i + 1}: ${todos[i]}`);
     }
-    // for (let task of todos) {
-    //   console.log(task);
-    // }
     console.log('********************');
   } else if (input === 'new') {
-    input = prompt('Ok, please add new to-do item:').toLowerCase();
-    todos.push(input);
-    console.log(`${input} added to the list!`);
+    const newTodo = prompt('Ok, please add new to-do item:').toLowerCase();
+    todos.push(newTodo);
+    console.log(`${newTodo} added to the list!`);
+  } else if (input === 'delete') {
+    const index = parseInt(prompt('Ok, enter an index to delete:'));
+    if (!Number.isNaN(index)) {
+      const deleted = todos.splice(index - 1, 1);
+      console.log(`Ok, deleted ${deleted}`)
+    } else {
+      console.log('Unknown index');
+    }
   }
   input = prompt('What would you like to do?').toLowerCase();
 }
