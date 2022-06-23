da9394a6302524571e360303741488e0a01a62c2
acea68 <alancea01@gmail.com>
2022-06-23 14:34:08 -0500

    updates longestSubstr

--- lengthOfLongestSubstring/index.js
+++ lengthOfLongestSubstring/index.js
@@ -28,30 +28,22 @@
 // c: include spaces, symbols, numbers as possible chars
 // e: include string length of 0 and large strings
 // -------------------
-// Soln #1:
-var lengthOfLongestSubstring = function (s) {
-  let map = {}
-  let start = 0
-  let maxLen = 0
-  let arr = s.split('')
-  for (i = 0; i < s.length; i++) {
-    let current = map[arr[i]]
-    if (current != null && start <= current) {
-      start = current + 1
-    } else {
-      maxLen = Math.max(maxLen, i - start + 1)
-    }
-    map[arr[i]] = i
+// Soln #2:
+function lengthOfLongestSubstring(s) {
+  let map = new Array(128).fill(0);
+  let right = 0, left = 0, result = 0, r, l;
+  while(right < s.length) {
+    r = s[right];
+    map
   }
-  return maxLen
-};
+}
 
 // TESTING
-// let result = lengthOfLongestSubstring("abca");
-// console.log(result); // 3
-// // Example 1:
-let result = lengthOfLongestSubstring("abcabcbb");
+let result = lengthOfLongestSubstring("abcaccrpk");
 console.log(result); // 3
+// // Example 1:
+// let result = lengthOfLongestSubstring("abcaccbb");
+// console.log(result); // 3
 // // Explanation: The answer is "abc", with the length of 3.
 
 // let result = lengthOfLongestSubstring("bbbbb");
@@ -91,3 +83,53 @@ console.log(result); // 3
 //   }
 //   return temp.length;
 // }
+
+// -------------------
+// Soln #1:
+// var lengthOfLongestSubstring = function (s) {
+//   let map = {};
+//   let start = 0;
+//   let maxLen = 0;
+//   let arr = s.split('');
+//   for (let i = 0; i < s.length; i++) {
+//     let current = map[arr[i]];
+//     if (current !== null && start <= current) {
+//       start = current + 1;
+//     } else {
+//       maxLen = Math.max(maxLen, i - start + 1);
+//     }
+//     map[arr[i]] = i;
+//   }
+//   return maxLen
+// };
+
+// -------------------
+// Soln #2:
+function lengthOfLongestSubstring(s) {
+  let map = new Array(128).fill(0);
+  // console.log(map)
+  let left = 0; // tracks left pointer index
+  let right = 0; // tracks right pointer index
+  let result = 0; // maxLength for substring
+  let r, l; //
+  // while the index on the right is less than the length of the string
+  while (right < s.length) {
+    // set r to the character of the input string at index of right pointer
+    r = s[right];
+    // add one to corresponding element in map to r's character unicode value
+    map[r.charCodeAt()] += 1;
+    // if this char has been seen already
+    while (map[r.charCodeAt()] > 1) {
+      // set l to the char at the left pointer index of input string
+      l = s[left];
+      // decrease map's element value by 1 corresponding to l's characted unicode value
+      map[l.charCodeAt()] -= 1;
+      // advance left index
+      left += 1;
+    } // set result to largest value between current result and delta window between right and left indexes (account for 0-index with +1)
+    result = Math.max(result, right - left + 1)
+    // advance right index
+    right += 1;
+  }
+  return result
+}
